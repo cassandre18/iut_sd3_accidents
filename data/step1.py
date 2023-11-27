00efb52a980acbe7f752e5de76857f38db256b11
cassandre18 <113589574+cassandre18@users.noreply.github.com>
2023-11-27 17:09:33 +0100

    commit 2

--- data/step1.py
+++ data/step1.py
@@ -1,7 +1,13 @@
 import pandas as pd
-carac=pd.read_csv('carac.csv', encoding='latin1')
-lieux=pd.read_csv('lieux.csv' , encoding='latin1')
-veh=pd.read_csv('veh.csv' , encoding='latin1')
-vict=pd.read_csv('carac.csv' , encoding='latin1')
+carac=pd.read_csv('c:\Users\cassa\Desktop\github_iut\iut_sd3_accidents\data\carac.csv', encoding='latin1')
+lieux=pd.read_csv('c:\Users\cassa\Desktop\github_iut\iut_sd3_accidents\data\data/lieux.csv' , encoding='latin1')
+veh=pd.read_csv('c:\Users\cassa\Desktop\github_iut\iut_sd3_accidents\data\data/veh.csv' , encoding='latin1')
+vict=pd.read_csv('c:\Users\cassa\Desktop\github_iut\iut_sd3_accidents\data\data/carac.csv' , encoding='latin1')
 
+data=pd.merge(carac,lieux,on='Num_Acc', how='inner')
+data=pd.merge(data,veh,on='Num_Acc', how='inner')
+data=pd.merge(data,vict,on='Num_Acc', how='inner')
 
+data.to_csv(data.csv,index=False, encoding='utf-8')
+
+print("Fusion terminéé, le fichier data.csv à été crée.")
