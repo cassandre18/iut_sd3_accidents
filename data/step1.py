712df8f91adf9cb5bb7570cd67c140f12f21e01d
cassandre18 <113589574+cassandre18@users.noreply.github.com>
2023-11-27 17:01:22 +0100

    commit

---
+++ data/step1.py
@@ -0,0 +1,7 @@
+import pandas as pd
+carac=pd.read_csv('carac.csv', encoding='latin1')
+lieux=pd.read_csv('lieux.csv' , encoding='latin1')
+veh=pd.read_csv('veh.csv' , encoding='latin1')
+vict=pd.read_csv('carac.csv' , encoding='latin1')
+
+
